ce10bc4c72b88efc937da891e9ed583fd2311627
Guilherme Felipe <guimota_silver@hotmail.com>
2023-03-28 17:44:30 -0300

    Finalizando projeto - 1

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,16 +5,7 @@ namespace MyRazorApp.Pages;
 
 public class Index : PageModel
 {
-    public List<Category> Categories { get; set; } = new ();
-
-    public async Task OnGet()
+    public void OnGet()
     {
-        await Task.Delay(5000);
-        for(int i = 0; i <= 100; i++)
-        {
-            Categories.Add(new Category(i, $"Categoria {i}", i * 18.95M));
-        }
     }
 }
-
-public record Category(int Id, string Title, decimal Price);
